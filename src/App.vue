a89c3e58d0b9c17c921b7592fd7c11e8fa1d86c7
秦晓琪 <3238509745@qq.com>
2026-07-25 18:27:51 +0800

    初始化完成：Cesium地球已跑通

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="cesium-container"></div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import * as Cesium from 'cesium'
+
+onMounted(() => {
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4NjFmMDgzNi0zZjkyLTQ1MWMtYWRlMy1mMGQ5ODgzMzJhZWIiLCJpZCI6NDYwMzg2LCJzdWIiOiJxaW54aWFvcWkiLCJpc3MiOiJodHRwczovL2FwaS5jZXNpdW0uY29tIiwiYXVkIjoicXhxIiwiaWF0IjoxNzg0OTczOTMzfQ.5z9EE082mukFuyQGbELti2_PclZRguiCsiwtAimwohI'
+  
+  new Cesium.Viewer('cesium-container', {
+    baseLayerPicker: false,
+    geocoder: false,
+    timeline: false,
+    animation: false,
+    sceneModePicker: false,
+  })
+})
+</script>
+
+<style scoped>
+#cesium-container {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
